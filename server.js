1f33feb1d90e490769ea41252380ab1742965edb
Rodrigo Araujo <dygufa@gmail.com>
2016-05-01 15:03:07 -0700

    Replacing mongoose for sequelize

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 /*
 Enviroment variable to be defined:
 
-- MONGO_URL: As it says... the mongo URL
+- PSQÇ_URL: As it says... the psql URL (it's something like postgres://user:pass@localhost/dbname)
 - PORT: The port where the application will be running
 - NODE_ENV: The enviroment running the application (dev or production)
 */
 
-require('./server/core/mongoose')
 require('./server/core/router')
--- server/core/router.js
+++ server/core/router.js
@@ -1,8 +1,10 @@
 var express = require('express'),
     app = express(),
-    fs = require('fs')
+    fs = require('fs'),
+    bodyParser = require('body-parser');
 
 var cwd = process.cwd();
+var models = require('../models')
 
 var controllers = {}, 
     controllers_path = cwd + '/server/controllers'
@@ -13,10 +15,16 @@ fs.readdirSync(controllers_path).forEach(function (file) {
     }
 })
 
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.use(express.static(cwd + '/public'));
 app.use('/node_modules', express.static(cwd + '/node_modules'));
 
 app.get('/files', controllers.files.getIndex)
+app.post('/files', controllers.files.addFile)
 
 app.use(function(req, res) {
     res.sendFile(cwd + '/public/index.html')
@@ -24,10 +32,13 @@ app.use(function(req, res) {
 
 var port = process.env.PORT || 5020;
 
-app.listen(port, function (err) {
-    if (err) {
-        console.error(err)
-    } else {
-        console.log('App is ready at : ' + port)
-    }
-})
+
+models.sequelize.sync().then(function() {
+    app.listen(port, function (err) {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log('App is ready at : ' + port)
+        }
+    })
+});
