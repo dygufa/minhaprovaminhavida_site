b3ef37d8e0c09ac371a5782dc7bf0922dd532602
Rodrigo Araujo <dygufa@gmail.com>
2016-05-15 21:19:06 -0700

    Informing the new env variables that must be declared

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 /*
 Enviroment variable to be defined:
 
-- PSQÇ_URL: As it says... the psql URL (it's something like postgres://user:pass@localhost/dbname)
+- PSQL_URL: As it says... the psql URL (it's something like postgres://user:pass@localhost/dbname)
 - PORT: The port where the application will be running
 - NODE_ENV: The enviroment running the application (dev or production)
+- AWS_ACCESS_KEY_ID 
+- AWS_SECRET_ACCESS_KEY
+- S3_BUCKET
 */
 
 require('./server/core/router')
