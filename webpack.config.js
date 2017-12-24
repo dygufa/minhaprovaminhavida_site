72c52295fe6bef2d34919a12f0563aafb7adff1f
Rodrigo Araujo <dygufa@gmail.com>
2017-12-23 21:48:26 -0300

    cleaning up and setting the right root

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,11 @@ const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 const ImageMinPlugin = require("imagemin-webpack-plugin").default;
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const shouldBuildForProduction =
-	process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging";
+const shouldBuildForProduction = process.env.NODE_ENV === "production";
 const hash = shouldBuildForProduction ? "[chunkhash]" : "[name].[hash]";
 
 const devServerHost = "localhost";
 const devServerPort = 6016;
-
-// const rootDir = path.resolve(__dirname);
 
 const cssLoader = (modules = false) => ({
 	loader: "css-loader",
@@ -30,187 +27,171 @@ const postCssLoader = {
 	},
 };
 
-const sassLoader = rootDir => ({
+const sassLoader = {
 	loader: "sass-loader",
 	options: {
-		includePaths: [path.resolve(rootDir, "style")],
+		includePaths: ["./src/style"],
 	},
-});
+};
 
 const styleLoader = "style-loader";
 
-function generateConfigForDir(rootDir) {
-	return {
-		cache: true,
+module.exports = {
+	cache: true,
+	entry: "./src/index",
+	output: {
+		pathinfo: true,
+		publicPath: "/",
+		filename: "bundle.[hash].js",
+		chunkFilename: `chunk.${hash}.js`,
+		path: path.join(__dirname, 'dist'),
+		sourceMapFilename: "bundle.[hash].js.map",
+	},
 
-		entry: [
-			...(shouldBuildForProduction
-				? []
-				: [
-					// "react-hot-loader/patch",
-					// "webpack/hot/only-dev-server",
-				]),
-			`${rootDir}/index`,
-		],
+	devtool: shouldBuildForProduction ? "source-map" : "source-map",
 
-		context: rootDir,
+	resolve: {
+		extensions: [".tsx", ".ts", ".js", ".jsx"],
+	},
 
-		output: {
-			pathinfo: true,
-			publicPath: "/",
-			filename: "bundle.[hash].js",
-			chunkFilename: `chunk.${hash}.js`,
-			path: path.join(rootDir, "/dist"),
-			sourceMapFilename: "bundle.[hash].js.map",
-		},
-
-		devtool: shouldBuildForProduction ? "source-map" : "source-map",
-
-		resolve: {
-			extensions: [".tsx", ".ts", ".js", ".jsx"],
-		},
-
-		module: {
-			rules: [
-				{
-					test: /react-icons.*\.js$/,
-					loader: "babel-loader",
-				},
-				{
-					test: /\.tsx?$/,
-					use: ["babel-loader", "awesome-typescript-loader"],
-				},
-				{
-					test: /\.css/,
-					use: ExtractTextPlugin.extract({
-						fallback: styleLoader,
-						use: [cssLoader(false), postCssLoader],
-					}),
-				},
-				{
-					test: /\.scss$/,
-					use: ExtractTextPlugin.extract({
-						fallback: styleLoader,
-						use: [
-							cssLoader(true),
-							postCssLoader,
-							sassLoader(rootDir),
-						],
-					}),
-				},
-				{
-					test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/,
+	module: {
+		rules: [
+			{
+				test: /react-icons.*\.js$/,
+				loader: "babel-loader",
+			},
+			{
+				test: /\.tsx?$/,
+				use: ["babel-loader", "awesome-typescript-loader"],
+			},
+			{
+				test: /\.css/,
+				use: ExtractTextPlugin.extract({
+					fallback: styleLoader,
+					use: [cssLoader(false), postCssLoader],
+				}),
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: styleLoader,
 					use: [
-						{
-							loader: "url-loader",
-							options: {
-								mimetype: "application/font-woff",
-							},
-						},
+						cssLoader(true),
+						postCssLoader,
+						sassLoader,
 					],
-				},
-				{
-					test: /\.(ttf|otf|eot)(\?v=[0-9].[0-9].[0-9])?$/,
-					loader: [
-						{
-							loader: "file-loader",
-							options: {
-								name: "[name].[ext]",
-							},
+				}),
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/,
+				use: [
+					{
+						loader: "url-loader",
+						options: {
+							mimetype: "application/font-woff",
 						},
-					],
-				},
-				{
-					test: /\.(jpe?g|png|gif|svg)$/i,
-					use: [
-						{
-							loader: "file-loader",
-							options: {
-								hash: "sha512",
-								digest: "hex",
-								name: "[name].[ext]",
-							},
+					},
+				],
+			},
+			{
+				test: /\.(ttf|otf|eot)(\?v=[0-9].[0-9].[0-9])?$/,
+				loader: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
 						},
-					],
-				},
-			],
-		},
-
-		plugins: [
-			new ExtractTextPlugin({
-				filename: "style-[contenthash].css",
-				disable: !shouldBuildForProduction,
-				allChunks: true,
-			}),
-			new webpack.ExtendedAPIPlugin(),
-			new webpack.DefinePlugin({
-				"process.env.NODE_ENV": JSON.stringify(
-					process.env.NODE_ENV || "development",
-				),
-			}),
-			new HtmlWebpackPlugin({
-				title: "MPMV",
-				filename: "index.html",
-				template: `${rootDir}/index.html`,
-			}),
-			...(shouldBuildForProduction
-				? [
-					// new FaviconsWebpackPlugin(`${dir}assets/favicon-ball.png`),
-					new webpack.HashedModuleIdsPlugin(),
-					new webpack.optimize.AggressiveMergingPlugin({
-						minSize: 1024 * 50,
-						maxSize: 1024 * 100,
-					}),
-					new webpack.optimize.CommonsChunkPlugin({
-						children: true,
-						minChunks: 2,
-						async: true,
-					}),
-					new webpack.LoaderOptionsPlugin({
-						minimize: true,
-						debug: false,
-					}),
-					new webpack.optimize.OccurrenceOrderPlugin(),
-					new webpack.optimize.UglifyJsPlugin({
-						sourceMap: false,
-						mangle: true,
-					}),
-					new ImageMinPlugin({
-						gifsicle: {
-							interlaced: true,
+					},
+				],
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							hash: "sha512",
+							digest: "hex",
+							name: "[name].[ext]",
 						},
-						optipng: {
-							enable: true,
-							optimizationLevel: 7,
-						},
-						pngquant: {
-							quality: "65-90",
-							speed: 4,
-						},
-						mozjpeg: {
-							progressive: true,
-							quality: 65,
-						},
-					}),
-				]
-				: [
-					// new webpack.NamedModulesPlugin(),
-					// new webpack.HotModuleReplacementPlugin()
-				]),
+					},
+				],
+			},
 		],
+	},
 
-		devServer: {
-			inline: true,
-			port: devServerPort,
-			host: devServerHost,
-			historyApiFallback: true,
-			disableHostCheck: true,   // That solved it
-		},
+	plugins: [
+		new ExtractTextPlugin({
+			filename: "style-[contenthash].css",
+			disable: !shouldBuildForProduction,
+			allChunks: true,
+		}),
+		new webpack.ExtendedAPIPlugin(),
+		new webpack.DefinePlugin({
+			"process.env.NODE_ENV": JSON.stringify(
+				process.env.NODE_ENV || "development",
+			),
+		}),
+		new HtmlWebpackPlugin({
+			title: "MPMV",
+			filename: "index.html",
+			template: `./src/index.html`,
+		}),
+		...(shouldBuildForProduction
+			? [
+				// new FaviconsWebpackPlugin(`${dir}assets/favicon-ball.png`),
+				new webpack.HashedModuleIdsPlugin(),
+				new webpack.optimize.AggressiveMergingPlugin({
+					minSize: 1024 * 50,
+					maxSize: 1024 * 100,
+				}),
+				new webpack.optimize.CommonsChunkPlugin({
+					children: true,
+					minChunks: 2,
+					async: true,
+				}),
+				new webpack.LoaderOptionsPlugin({
+					minimize: true,
+					debug: false,
+				}),
+				new webpack.optimize.OccurrenceOrderPlugin(),
+				new webpack.optimize.UglifyJsPlugin({
+					sourceMap: false,
+					mangle: true,
+				}),
+				new ImageMinPlugin({
+					gifsicle: {
+						interlaced: true,
+					},
+					optipng: {
+						enable: true,
+						optimizationLevel: 7,
+					},
+					pngquant: {
+						quality: "65-90",
+						speed: 4,
+					},
+					mozjpeg: {
+						progressive: true,
+						quality: 65,
+					},
+				}),
+			]
+			: [
+				// new webpack.NamedModulesPlugin(),
+				// new webpack.HotModuleReplacementPlugin()
+			]),
+	],
 
-		node: {
-			fs: "empty",
-		},
-	};
-}
+	devServer: {
+		inline: true,
+		port: devServerPort,
+		host: devServerHost,
+		historyApiFallback: true,
+		disableHostCheck: true,
+	},
 
-module.exports = generateConfigForDir(path.resolve("./src/"));
+	node: {
+		fs: "empty",
+	},
+};
